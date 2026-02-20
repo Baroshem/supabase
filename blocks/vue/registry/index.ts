5396956324ab087377f6743eb255df2692697a7c
Baroshem <jakub.andrzejewski.dev@gmail.com>
2026-02-20 08:00:06 +0100

    docs: realtime chat vue&nuxt

--- blocks/vue/registry/index.ts
+++ blocks/vue/registry/index.ts
@@ -5,7 +5,8 @@ import { realtimeCursor } from './realtime-cursor'
 import { socialAuth } from './social-auth'
 import { currentUserAvatar } from './current-user-avatar'
 import { realtimeAvatarStack } from './realtime-avatar-stack'
+import { realtimeChat } from './realtime-chat'
 
-const blocks = [...clients, ...passwordBasedAuth, ...socialAuth, ...dropzone, ...realtimeCursor, ...currentUserAvatar, ...realtimeAvatarStack]
+const blocks = [...clients, ...passwordBasedAuth, ...socialAuth, ...dropzone, ...realtimeCursor, ...currentUserAvatar, ...realtimeAvatarStack, ...realtimeChat]
 
 export { blocks }
